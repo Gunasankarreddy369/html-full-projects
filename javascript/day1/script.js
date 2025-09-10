1f9ea46d5cc44e988b5bcd910a99739ffae1b3c6
Gunasankarreddy369 <gunasankarreddygudala@gmail.com>
2025-09-10 17:40:28 +0530

    updat

--- javascript/day1/script.js
+++ javascript/day1/script.js
@@ -1,32 +1,6 @@
 let a=+prompt("Enter a value ");
-let b=+prompt("Enter b value ");
-
-let z=true;
-while(true){
-let x=prompt("enter the operator");
-if(x=="+"){
-console.log("The sum of numbers",a+b)
-
-}else if(x=="-"){
-
-console.log("The diff of numbers",a-b)
 
 
-}else if(x=="*"){
-
-
-
-console.log("The mult of numbers",a*b)
-
-
-}else if(x=="/"){
-
-console.log("The div of numbers",a/b)
-20
-}
-else{
-    z=false;
-    break;
-}
-}
+let r=3.14*a*a;
+console.log(r);
 
